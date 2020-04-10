46027f7ad19c2eba867e27e062eb9665c1d7ddc5
Artem Kovtun <rtmkovtn@ya.ru>
2020-04-10 10:20:55 +0800

    moved main router to routes. Changed all routes to /api/

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const loginRouter = require('./routes/loginRouter');
-const auth = require('./middlewares/auth');
 const mainRouter = require('./routes/index');
 const wrongAddress = require('./routes/wrongAddress');
 const { errorsHandler } = require('./middlewares/errorsHandler');
@@ -34,12 +32,8 @@ app.listen(config.PORT, () => {});
 app.use(config.limiter); // antiDDOS limiter
 // logging all requests to request.loq file
 app.use(requestLogger);
-
-app.use('/', loginRouter);
-// authorization to wright token into cookies.jwt
-app.use(auth);
-
-app.use(mainRouter);
+// login, auth and all other router
+app.use('/api', mainRouter);
 // logging all errors to error.log file
 app.use(errorLogger);
 // errors handlers
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const articlesRouter = require('./articles');
+const auth = require('../middlewares/auth');
+const loginRouter = require('./loginRouter');
 
+router.use('/', loginRouter);
+router.use('/', auth);
 router.use('/', usersRouter, articlesRouter);
 
 module.exports = router;
