009a1ed31733ba6564fdef34d5657947067f83e6
Artem Kovtun <rtmkovtn@ya.ru>
2020-05-23 18:44:09 +0800

    -

--- src/app.js
+++ src/app.js
@@ -5,8 +5,13 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const cors = require('cors');
-const whitelist = ['http://localhost:8080', 'https://fckxyz.github.io', 'https://newsxyz.xyz', 'https://www.newsxyz.xyz'];
+// const cors = require('cors');
+// const whitelist = [
+//   'http://localhost:8080',
+//   'https://fckxyz.github.io',
+//   'https://newsxyz.xyz',
+//   'https://www.newsxyz.xyz',
+// ];
 
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -18,20 +23,20 @@ const config = require('./config.js');
 const app = express();
 
 // var allowCrossDomain = function(req, res, next) {
-   //  res.header('Access-Control-Allow-Origin', "*");
-    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//  res.header('Access-Control-Allow-Origin', "*");
+// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 //     res.header('Access-Control-Allow-Headers', 'Content-Type');
 //     next();
 // };
 // app.configure(function() {
-// 	app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
 // });
 // const corsOptions = {
-// 	credentials: true,
+// credentials: true,
 // origin: (origin, callback) => {
 //     if(whitelist.includes(origin))
 //       return callback(null, true)
-// 
+//
 //       callback(new Error('Not allowed by CORS'));
 //   }
 // };
@@ -41,11 +46,12 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(config.MONGO_DB_ADDRESS, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
+mongoose
+  .connect(config.MONGO_DB_ADDRESS, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .catch((err) => {
     console.error(err);
   });
