3d952de8ede2621feb55ffcfd9914627ce9be370
Artem Kovtun <rtmkovtn@ya.ru>
2020-05-24 11:43:05 +0800

    IDK

--- src/app.js
+++ src/app.js
@@ -23,7 +23,12 @@ const config = require('./config.js');
 const app = express();
 
 const allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', [
+    'http://localhost:8080',
+    'https://newsxyz.xyz',
+    'https://www.newsxyz.xyz',
+    'https://fckxyz.github.io/news-explorer-frontend',
+  ]);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
